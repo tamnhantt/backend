50ee266977f4f5b800c74994ce2bbf354223217e
Tanphat241-glich <tanphat12233hlk@gmail.com>
2025-05-23 09:55:49 +0700

    no1

--- ras_stm/send_try.py
+++ ras_stm/send_try.py
@@ -260,7 +260,7 @@ def receive_data():
             if typeid == IN_WHEEL_SPD_ID:
                 packed = struct.pack('>I', value)  # '>I' = big-endian unsigned int (4 byte)
                 value_bytes = list(packed)        # [0x00, 0x00, 0x0B, 0xB8]
-                bus.write_i2c_block_data(0x12, 0x00, [0, typeid] + value_bytes)
+                bus.write_i2c_block_data(0x12, 0x00, [0, typeid, value_bytes])
                 print(value)
             else:  
                 bus.write_i2c_block_data(0x12, 0x00, [0, typeid, value])
